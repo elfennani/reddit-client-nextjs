0cad0225b7b07bc12f2e3e508d133626f3867ac6
Nizar Elfennani <elfennani2002@gmail.com>
2022-09-07 17:08:51 +0100

    Updated PostView component

--- components/PostView/index.jsx
+++ components/PostView/index.jsx
@@ -3,12 +3,27 @@ import PropTypes from "prop-types";
 import styles from "./PostView.module.scss";
 import Card from "../Card";
 import { parseDate } from "../../utils/functions";
+import {
+    CaretDownOutlined,
+    CaretUpOutlined,
+    LogoutOutlined,
+    SaveFilled,
+    ShareAltOutlined,
+} from "@ant-design/icons";
+import Link from "next/link";
 
 /**
  * @typedef PostProps
- * @property {{name: string, picture: string}} subreddit
+ * @property {{name: string, picture: string, link: string}} subreddit
  * @property {string} creator
  * @property {number} createdOn
+ * @property {string} creatorLink
+ * @property {number} votes
+ * @property {number} commentCount
+ * @property {string} image
+ * @property {string} title
+ * @property {string} text
+ * @property {string} extPostLink
  */
 
 /**
@@ -18,17 +33,72 @@ import { parseDate } from "../../utils/functions";
  */
 const PostView = (props) => {
     return (
-        <Card>
+        <Card className={styles.container}>
             <header>
                 <img src={props.subreddit.picture} alt={props.subreddit.name} />
                 <div>
-                    <h2>{props.subreddit.name}</h2>
+                    <Link href={props.subreddit.link}>
+                        <a>
+                            <h2>{props.subreddit.name}</h2>
+                        </a>
+                    </Link>
                     <p>
-                        Posted by {props.creator} {"\u2022"}{" "}
-                        {parseDate(props.createdOn)}
+                        Posted by{" "}
+                        <Link href={props.creatorLink}>
+                            <a>{props.creator}</a>
+                        </Link>{" "}
+                        {"\u2022"} {parseDate(props.createdOn)}
                     </p>
                 </div>
+                {props.extPostLink && (
+                    <Link href={props.extPostLink}>
+                        <a>
+                            <LogoutOutlined rotate={-45} />
+                        </a>
+                    </Link>
+                )}
             </header>
+            <div className={styles.content}>
+                {props.image && <img src={props.image} alt={props.title} />}
+                <h2>{props.title}</h2>
+                {props.text && <p className={styles.text}>{props.text}</p>}
+            </div>
+            <div className={styles.toolbar}>
+                <p>
+                    <span>
+                        {props.votes} vote
+                        {props.votes.toString()[
+                            props.votes.toString().length - 1
+                        ] === "1"
+                            ? ""
+                            : "s"}
+                    </span>{" "}
+                    • {props.commentCount} comment
+                    {props.commentCount.toString()[
+                        props.commentCount.toString().length - 1
+                    ] === "1"
+                        ? ""
+                        : "s"}
+                </p>
+                <div className={styles.buttonRow}>
+                    <div>
+                        <button>
+                            <CaretUpOutlined />
+                        </button>
+                        <button>
+                            <CaretDownOutlined />
+                        </button>
+                    </div>
+                    <div>
+                        <button>
+                            <ShareAltOutlined /> <p>Share</p>
+                        </button>
+                        <button>
+                            <SaveFilled /> <p>Save</p>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </Card>
     );
 };
@@ -37,9 +107,11 @@ PostView.propTypes = {
     subreddit: PropTypes.shape({
         name: PropTypes.string,
         picture: PropTypes.string,
+        link: PropTypes.string,
     }).isRequired,
     creator: PropTypes.string,
     createdOn: PropTypes.number,
+    creatorLink: PropTypes.string,
     title: PropTypes.string,
     text: PropTypes.string,
     image: PropTypes.string,
--- stories/PostView.stories.js
+++ stories/PostView.stories.js
@@ -4,7 +4,12 @@ import PostView from "../components/PostView";
 export default {
     title: "Post",
     component: PostView,
-    argTypes: {},
+    argTypes: {
+        onUpvote: { action: "onUpvote" },
+        onDownvote: { action: "onDownvote" },
+        onSave: { action: "onSave" },
+        onShare: { action: "onShare" },
+    },
 };
 
 const Template = (args) => <PostView {...args} />;
@@ -12,8 +17,20 @@ const Template = (args) => <PostView {...args} />;
 export const Default = Template.bind({});
 Default.args = {
     subreddit: {
-        name: "u/funny",
+        name: "r/funny",
         picture:
             "https://styles.redditmedia.com/t5_2tex6/styles/communityIcon_u89jf60zv7p41.png",
+        link: "https://www.reddit.com/r/funny/",
     },
+    createdOn: 1660055529,
+    creator: "u/elfennani",
+    creatorLink: "https://www.reddit.com/user/elfennani",
+    title: "Purpolch with a subtle purple cable from Mechcables.",
+    // text: "",
+    image: "https://images.unsplash.com/photo-1560762484-813fc97650a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    votes: 420,
+    commentCount: 69,
+    extPostLink:
+        "https://www.reddit.com/r/Frontend/comments/x7hgph/what_are_some_common_programming_tasks_for_front/?utm_source=share&utm_medium=web2x&context=3",
+    type: "image",
 };
