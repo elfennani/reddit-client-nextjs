ede1069129dc55d2554cdc10dd3f1118e209a369
Nizar Elfennani <elfennani2002@gmail.com>
2022-11-07 09:52:23 +0100

    Added ENV variables

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,6 @@
 export default {
-    clientId: "DWY9af0IjWnRpN6wT0t0dg",
-    secretId: "HOdNt-cApn886xJs4OxhlRL114xveg",
+    clientId: process.env.CLIENT_ID || "DWY9af0IjWnRpN6wT0t0dg",
+    secretId: process.env.CLIENT_SECRET || "HOdNt-cApn886xJs4OxhlRL114xveg",
     redirectUri: "http://localhost:3000/authenticate",
     basicCredentials: Buffer.from(
         "DWY9af0IjWnRpN6wT0t0dg:HOdNt-cApn886xJs4OxhlRL114xveg"
